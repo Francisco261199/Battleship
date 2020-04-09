f12e57597ad8625f76256e076dc2bb364633860a
Francisco261199 <61276806+Francisco261199@users.noreply.github.com>
2020-04-09 16:59:09 +0100

    Add files via upload

---
+++ templates.h
@@ -0,0 +1,11 @@
+typedef struct{
+    char bitmap[5][5];
+    int size;
+}SHIP;
+
+extern SHIP *Pickaxe;
+extern SHIP *Carrier;
+extern SHIP *Battleship;
+extern SHIP *Submarine;
+extern SHIP *Cruiser;
+extern SHIP *Destroyer;
