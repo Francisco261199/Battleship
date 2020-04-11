cbc380fe898680dd436ac3a03168c5f77273b2e5
Francisco261199 <61276806+Francisco261199@users.noreply.github.com>
2020-04-11 18:55:25 +0100

    Update templates.c

--- templates.c
+++ templates.c
@@ -3,76 +3,77 @@
 #include<stdlib.h>
 
 //Create variables in memory
-static SHIP const Pickaxe_data =
+static SHIP  Pickaxe_data =
 {
 .size = 9,
 .bitmap = { {'0', '1', '1', '1', '0'}, //
             {'1', '0', '1', '0', '1'}, //
             {'0', '0', '1', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
-            {'0', '0', '1', '0', '0'}
+            {'0', '0', '1', '0', '0'}  //
           }
 };
 
-static SHIP const Carrier_data =
+static SHIP  Sigma_data =
+{
+.size = 7,
+.bitmap = { {'0', '0', '1', '1', '0'}, //
+            {'0', '0', '1', '0', '0'}, //
+            {'0', '0', '1', '0', '0'}, //
+            {'0', '0', '1', '0', '0'}, //
+            {'0', '1', '1', '0', '0'}  //
+          }
+};
+
+static SHIP  Carrier_data =
 {
 .size = 5,
 .bitmap = { {'0', '0', '1', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
-            {'0', '0', '1', '0', '0'}
+            {'0', '0', '1', '0', '0'}  //
           }
 };
 
-static SHIP const Battleship_data =
+static SHIP  Battleship_data =
 {
 .size = 4,
 .bitmap = { {'0', '0', '1', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
-            {'0', '0', '0', '0', '0'}
+            {'0', '0', '0', '0', '0'}  //
           }
 };
 
-static SHIP const Submarine_data =
+static SHIP  Cruiser_data =
 {
 .size = 3,
 .bitmap = { {'0', '0', '0', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
-            {'0', '0', '0', '0', '0'}
+            {'0', '0', '0', '0', '0'}  //
           }
 };
 
-static SHIP const Cruiser_data =
-{
-.size = 3,
-.bitmap = { {'0', '0', '0', '0', '0'}, //
-            {'0', '0', '1', '0', '0'}, //
-            {'0', '0', '1', '0', '0'}, //
-            {'0', '0', '1', '0', '0'}, //
-            {'0', '0', '0', '0', '0'}
-          }
-};
-
-static SHIP const Destroyer_data =
+static SHIP  Destroyer_data =
 {
 .size = 2,
 .bitmap = { {'0', '0', '0', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
             {'0', '0', '1', '0', '0'}, //
             {'0', '0', '0', '0', '0'}, //
-            {'0', '0', '0', '0', '0'}
+            {'0', '0', '0', '0', '0'}  //
           }
 };
 
 //assign data to each respective SHIP
-const SHIP* const Pickaxe = &Pickaxe_data;
-const SHIP* const Carrier = &Carrier_data;
-const SHIP* const Battleship = &Battleship_data;
-const SHIP* const Submarine = &Submarine_data;
-const SHIP* const Cruiser = &Cruiser_data;
-const SHIP* const Destroyer = &Destroyer_data;
+ SHIP*  Pickaxe = &Pickaxe_data;
+ SHIP*  Sigma = &Sigma_data;
+ SHIP*  Carrier = &Carrier_data;
+ SHIP*  Battleship = &Battleship_data;
+ SHIP*  Cruiser = &Cruiser_data;
+ SHIP*  Destroyer = &Destroyer_data;
+
