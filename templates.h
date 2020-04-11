a0c14986398a7a22f83ff31e05eb96efbd5c8dac
Francisco261199 <61276806+Francisco261199@users.noreply.github.com>
2020-04-11 22:49:56 +0100

    Update templates.h

--- templates.h
+++ templates.h
@@ -1,11 +1,15 @@
 typedef struct{
+    //center coordinates of the boat in CELL
+    int x;
+    int y;
     char bitmap[5][5];
     int size;
 }SHIP;
 
-extern SHIP *Pickaxe;
-extern SHIP *Carrier;
-extern SHIP *Battleship;
-extern SHIP *Submarine;
-extern SHIP *Cruiser;
-extern SHIP *Destroyer;
+
+extern  SHIP*  Pickaxe;
+extern  SHIP*  Carrier;
+extern  SHIP*  Battleship;
+extern  SHIP*  Sigma;
+extern  SHIP*  Cruiser;
+extern  SHIP*  Destroyer;
