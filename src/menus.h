4cdbbfcd77a1862346de7fb86159a34b7e08b46b
arcmarqs <maramadeu@gmail.com>
2020-04-30 12:25:50 +0100

    Update menus.h

--- src/menus.h
+++ src/menus.h
@@ -5,9 +5,28 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<ncurses.h>
+#include"ships.h"
+#include"board.h"
 
 
+// MACROS
+
+#define START_MENU 0
+#define START_GAME 1
+#define CREDITS 2
+#define LEAVE 3
+#define BACK 4
+#define START_GAME_END 5
+#define RANDOM_INSERTION 6
+#define SELECTED_INSERTION 7
+#define ERROR 404
+
+
+//variables
+extern int n_boats,map_size,rand_flag;
+extern int n_for_each_boat[6];
+
+//functions
 void menu_select();
-void prints_ships();
 
 #endif
