79e4ab29fd51182afe5e78fc094d6c8b95948869
Francisco261199 <61276806+Francisco261199@users.noreply.github.com>
2020-05-02 19:24:08 +0100

    Update board.c

--- src/board.c
+++ src/board.c
@@ -31,13 +31,12 @@ GAME * init_board(int size){
 // inserção do navio no tabuleiro
 int verify_insert(int x, int y, SHIP* ship, CELL**map, int map_size){
   if(x>(map_size-1) || y>(map_size-1)) return 1;
-
-  int clear = 0;
+  int flag,clear;
+  clear = 0;
   for(int i=0;i<5;i++){
     for(int j=0;j<5;j++){
-      int flag = 1;
-
-      //able to place boats in extremity of map even if bitmap doesnt fit
+      flag = 1;
+      //able to place boats in extremity of map even if bitmap doesn't fit
       if( ((x+i-2) >= map_size || (y+j-2) >= map_size || (x+i-2) < 0 || (y+j-2) < 0 ) && ship->bitmap[i][j] == EMPTY) flag=0;
       //out of bounds
       else if( ((x+i-2) >= map_size || (y+j-2) >= map_size || (x+i-2) < 0 || (y+j-2) < 0 ) && ship->bitmap[i][j] != EMPTY) return 1;
@@ -51,13 +50,14 @@ int verify_insert(int x, int y, SHIP* ship, CELL**map, int map_size){
 void insert_ship(int x,int y,SHIP* ship,CELL** map, int map_size){
   int check = verify_insert(x,y,ship,map,map_size);
   if(check != ship->size){
-    //if(check == 1) printf("Boat out of bounds!\n");
+
     printf("\033[1;31m");
     printf("ERROR! You can't insert the boat here!\n");
     printf("\033[1;33m");
     printf("Please choose another position.\n");
     printf("X: "); scanf("%d",&x);
-    printf("Y: "); scanf("%d",&y); printf("\n");
+    printf("Y: "); scanf("%d",&y);
+    printf("\n");
 
     insert_ship(x,y,ship,map,map_size);
   }
@@ -79,12 +79,11 @@ void insert_ship(int x,int y,SHIP* ship,CELL** map, int map_size){
 void user_insert(GAME* g){
   int boat_types[]={2,3,4,5,7,9};
   char *boats[]={"Destroyer","Cruiser","Battleship","Carrier","Sigma","Pickaxe"};
-  int x,y,boat,rotation;
+  int x,y,boat,rotation,boat_rotation,player;
+  player = 1;
   system("clear");
-  printf("Time to insert your boats!\n");
   printf("\033[1;31m");
-  printf("Remember: X->rows, Y->cols, (y,x)->boat's center\n");
-  printf("For rotations you have:\n");
+  printf("Time to insert your boats!\nRemember: \nX->rows, Y->cols, (y,x)->boat's center\nFor rotations you have:\n");
   printf(" ___________\n");
   printf("|           |\n");
   printf("| 0 -> 0º   |\n");
@@ -94,146 +93,46 @@ void user_insert(GAME* g){
   printf("|___________|\n");
   printf("Press any key to ENTER");
   getchar();
-  int player = 1;
-  int boat_rotation;
+
   for(;player<=2;player++){
-    boat_rotation = 0;
     for(int i=0;i<6;i++){
       for(int j=0;j<n_for_each_boat[i];j++){
         system("clear");
         SHIP* newship = (SHIP*) malloc(sizeof(SHIP));
-        if(player == 1){ //player 1 inserts
-          print_game(g->map1,g->size);
-          printf("\033[1;35m");
-          printf("               Player %d: \n",player);
-          printf("\033[1;33m");
-          printf("|--------------------------------------|\n");
-          printf("%s\n",(char*)boats[i]);
 
-          printf("Select Rotation: ");scanf("%d",&boat_rotation);
-          //get valid rotation
-          while(boat_rotation < 0 || boat_rotation > 3){
-            printf("\033[1;31m");
-            printf("Invalid rotation. Insert new one: ");
-            printf("\033[1;33m");
-            scanf("%d",&boat_rotation);
-          }
-          switch(i){
-            case 0:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map1,g->size);
-              break;
-            case 1:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map1,g->size);
-              break;
-            case 2:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map1,g->size);
-              break;
-            case 3:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map1,g->size);
-              break;
-            case 4:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map1,g->size);
-              break;
-            case 5:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map1,g->size);
-              break;
-          }
+        //print map during insertion
+        if(player == 1) print_game(g->map1,g->size);
+        else print_game(g->map2,g->size);
+
+        printf("\033[1;35m");
+        printf("               Player %d: \n",player);
+        printf("\033[1;33m");
+        printf("|--------------------------------------|\n");
+        printf("%s\n",(char*)boats[i]);
+
+        //get valid rotation
+        printf("Select Rotation: ");
+        scanf("%d",&boat_rotation);
+        while(boat_rotation < 0 || boat_rotation > 3){
+          printf("\033[1;31m");
+          printf("Invalid rotation. Insert new one: ");
+          printf("\033[1;33m");
+          scanf("%d",&boat_rotation);
         }
-        else{ //player2 inserts
-          print_game(g->map2,g->size);
-          printf("\033[1;35m");
-          printf("               Player %d: \n",player);
-          printf("\033[1;33m");
-          printf("|--------------------------------------|\n");
-          printf("%s\n",(char*)boats[i]);
+        printf("X:");
+        scanf("%d",&x);
+        printf("Y:");
+        scanf("%d",&y);
 
-          printf("Select Rotation: ");scanf("%d",&boat_rotation);
-          //get valid rotation
-          while(boat_rotation < 0 || boat_rotation > 3){
-            printf("\033[1;31m");
-            printf("Invalid rotation. Insert new one: ");
-            printf("\033[1;33m");
-            scanf("%d",&boat_rotation);
-          }
-          switch(i){
-            case 0:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map2,g->size);
-              break;
-            case 1:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map2,g->size);
-              break;
-            case 2:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map2,g->size);
-              break;
-            case 3:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map2,g->size);
-              break;
-            case 4:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map2,g->size);
-              break;
-            case 5:
-              printf("X:");
-              scanf("%d",&x);
-              printf("Y:");
-              scanf("%d",&y);
-              create_ship(newship,boat_rotation,boat_types[i]);
-              insert_ship(x,y,newship,g->map2,g->size);
-              break;
-          }
+        //player 1 inserts('insert_ship' asks for new coordinates if needed)
+        if(player == 1){
+          create_ship(newship,boat_rotation,boat_types[i]);
+          insert_ship(x,y,newship,g->map1,g->size);
+        }
+        //player2 inserts('insert_ship' asks for new coordinates if needed)
+        else{
+          create_ship(newship,boat_rotation,boat_types[i]);
+          insert_ship(x,y,newship,g->map2,g->size);
         }
       }
     }
@@ -248,40 +147,44 @@ void rand_insert_ships(GAME* b){
   int boat_types[]={2,3,4,5,7,9};
   int x1,x2,y1,y2,map_size,rotation,verify;
   map_size = b->size;
+
   for(int i=0;i<6;i++){
     for(int j=0;j<n_for_each_boat[i];j++){
+
+      //create ships
       SHIP* newship1 = (SHIP*) malloc(sizeof(SHIP));
       SHIP* newship2 = (SHIP*) malloc(sizeof(SHIP));
-      x1 = x2 = y1 = y2 = 0;
       rotation = generate_number(0,3);
+      create_ship(newship1,rotation,boat_types[i]);
+      create_ship(newship2,rotation,boat_types[i]);
+
+      //get random coordinates
       x1 = generate_number(0,(map_size-1));
       y1 = generate_number(0,(map_size-1));
       x2 = generate_number(0,(map_size-1));
       y2 = generate_number(0,(map_size-1));
 
-      create_ship(newship1,rotation,boat_types[i]);
-      create_ship(newship2,rotation,boat_types[i]);
+      //verify if insertion is possible
       while((verify=verify_insert(x1,y1,newship1,b->map1,map_size)) != newship1->size){
         x1 = generate_number(0,(map_size-1));
         y1 = generate_number(0,(map_size-1));
-        //printf("2- x1=%d, y1=%d, x2=%d, y2=%d\n",x1,y1,x2,y2);
       }
 
       while((verify=verify_insert(x2,y2,newship2,b->map2,map_size)) != newship2->size){
         x2 = generate_number(0,(map_size-1));
         y2 = generate_number(0,(map_size-1));
-      //printf("3- x1=%d, y1=%d, x2=%d, y2=%d\n",x1,y1,x2,y2);
       }
+
+      //insert
       insert_ship(x1,y1,newship1,b->map1,map_size);
       insert_ship(x2,y2,newship2,b->map2,map_size);
     }
   }
 }
 
-//atacar navio
+//atack ship
 int attack(int x,int y, CELL **map,int size){
-  //permitir ao jogador selecionar nova posição
-  //no caso de este ter inserido posição fora do board
+  //ask for new coordinates if user selects out of bounds position
   if(x>(size-1) || y>(size-1)){
     do{
       printf("\033[1;31m");
@@ -292,14 +195,15 @@ int attack(int x,int y, CELL **map,int size){
     }while(x>(size-1) || y>(size-1));
   }
 
+  printf("\033[1;36m");
 
-
+  //has ship
   if(map[x][y].ship != NULL){
+
     //conversion to bitmap coordinates(map(x,y)->bitmap(x,y))
     int bitmap_x = 2+(x-map[x][y].ship->x);
     int bitmap_y = 2+(y-map[x][y].ship->y);
-    //printf("xmap:%d ymap:%d\n",map[x][y].ship->x,map[x][y].ship->y);
-    //printf("x:%d, y:%d\n",bitmap_x,bitmap_y);
+
     //boat piece not hit
     if(map[x][y].ship->bitmap[bitmap_x][bitmap_y] == NOT_HIT){
       //update bitmap
@@ -308,14 +212,16 @@ int attack(int x,int y, CELL **map,int size){
       map[x][y].shot = _HIT_CELL;
       map[x][y].ship->size -= 1;
       printf("Hit!\n");
+
+      //ship fully destroyed
       if(map[x][y].ship->size == 0){
         printf("Ship Destroyed!\n");
         return 1;
-
       }
       return 0;
     }
-    //boat piece hit
+
+    //boat piece already hit
     if(map[x][y].ship->bitmap[bitmap_x][bitmap_y] == HIT){
       printf("Already hit(with boat)! Please try again\n");
       //get new coodinates
@@ -325,9 +231,10 @@ int attack(int x,int y, CELL **map,int size){
       return 0;
     }
   }
-  //already hit position and no boat
+
+  //no boat(water)
   else{
-    //water and non-attacked
+    //non-attacked position
     if(map[x][y].ship == NULL && map[x][y].shot == _NO_SHOT){
       printf("Miss!No Boat!\n");
       printf("\n");
@@ -335,7 +242,7 @@ int attack(int x,int y, CELL **map,int size){
       map[x][y].shot = _MISSED_SHOT;
       return 0;
     }
-    //water and attacked
+    //already attacked position
     if(map[x][y].shot == _MISSED_SHOT){
       printf("Already hit(without boat)! Please try again\n");
       //get new coodinates
@@ -411,22 +318,19 @@ void print_secret_board(CELL **map,int size){
 }
 
 GAME* erase_game(GAME* board){
+  for(int i=0;i<board->size;i++){
+    free(board->map1[i]);
+    free(board->map2[i]);
+    board->map1[i]=NULL;
+    board->map2[i]=NULL;
+  }
 
+  free(board->map1);
+  free(board->map2);
 
-for(int i=0;i<board->size;i++){
-  free(board->map1[i]);
-  free(board->map2[i]);
-  board->map1[i]=NULL;
-  board->map2[i]=NULL;
-}
+  board->map1=NULL;
+  board->map2=NULL;
 
-free(board->map1);
-free(board->map2);
-
-board->map1=NULL;
-board->map2=NULL;
-
-free(board);
-
-return NULL;
+  free(board);
+  return NULL;
 }
