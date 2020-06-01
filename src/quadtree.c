c04217c9281a7e6ad9c7e4e4531d467a375191f1
arcmarqs <maramadeu@gmail.com>
2020-06-01 16:23:46 +0100

    Add files via upload

--- src/quadtree.c
+++ src/quadtree.c
@@ -1,73 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-//#include "quadtree.h"
+#include "quadtree.h"
 #include "ships.h"
-#include<time.h>
 
-//Flags for bitmap
-#define EMPTY '0'
-#define NOT_HIT '1'
-#define HIT '2'
-
-//flags for CELL maps
-#define _NO_SHOT '0'
-#define _NO_HIT '1'
-#define _HIT_CELL '2'
-#define _MISSED_SHOT '3'
-
-typedef struct POINT{
-  int x,y;
-}POINT;
-
-typedef enum { QDNODE, QDLEAF} QD_TNODE;
-
-typedef struct QD_Node_ {
-  QD_TNODE  type;
-  int nodes_inside;
-  float cx,cy,level;
-  struct QD_Node_* parent;
-  union {
-    struct QD_Node_*quadrants[4];    // internal node
-    struct {
-      SHIP* ship;  // apontador barco
-      POINT* p;     // (x,y) board
-    } leaf;        //  cell version for qtree
-  } node;
-} QD_NODE;
-
-typedef struct{
-  QD_NODE* root;
-  char shot;
-} CELL;
-
-typedef struct{
-    CELL **map1;
-    CELL **map2;
-    int size;
-    int state1,state2;
-} GAME;
-
-// create board in memory
-GAME * init_board(int size){
-    GAME * pGame = (GAME*)malloc(sizeof(GAME));
-
-    pGame->size = size;
-    pGame->map1 = (CELL**)malloc(size*sizeof(CELL));
-    pGame->map2 = (CELL**)malloc(size*sizeof(CELL));
-
-    for(int i=0;i<size;i++){
-        pGame->map1[i] = (CELL*)malloc(size*sizeof(CELL));
-        pGame->map2[i] = (CELL*)malloc(size*sizeof(CELL));
-        for(int j=0;j<size;j++){
-
-          pGame->map1[i][j].shot= _NO_SHOT;
-          pGame->map1[i][j].ship = NULL;
-          pGame->map2[i][j].shot= _NO_SHOT;
-          pGame->map2[i][j].ship = NULL;
-        }
-      }
-  return pGame;
-}
 
 //create point
 POINT * make_point(int x,int y){    //constrói o ponto (x,y) e o retorna;
@@ -417,7 +352,8 @@ int search_point(POINT* p,QD_NODE* root){
 }
 
 
-/*POINT* create_points(POINT* p, SHIP* ship){
+/*
+POINT* create_points(POINT* p, SHIP* ship){
   int index=0;
   POINT* ans= (POINT*) malloc(ship->size*sizeof(POINT));
   for(int i=0;i<5;i++){
@@ -430,210 +366,12 @@ int search_point(POINT* p,QD_NODE* root){
     }
   }
   return ans;
-}*/
+}
 
 // inserção do navio no tabuleiro
-int verify_insert(QD_NODE* insert, QD_NODE* root, POINT* points){
-  int x = insert->node.leaf.p->x;
-  int y = insert->node.leaf.p->y;
-  if(x>(root->level) || y>(root->level)) return 1;
-  int flag,clear;
-  clear = 0;
-  QD_NODE* aux = create_node(root->level/2.0);
-  for(int i=0;i<5;i++){
-    for(int j=0;j<5;j++){
-      flag = 1;
-      //able to place boats in extremity of map even if bitmap doesn't fit
-      if( ((x+i-2) > root->level || (y+j-2) > root->level || (x+i-2) < 0 || (y+j-2) < 0 ) &&
-            insert->node.leaf.ship->bitmap[i][j] == EMPTY) flag=0;
-      //out of bounds
-      else if( ((x+i-2) > root->level || (y+j-2) > root->level || (x+i-2) < 0 || (y+j-2) < 0 ) &&
-                insert->node.leaf.ship->bitmap[i][j] != EMPTY) return 1;
-      //insert is possible for given position
-       aux = get_subdivision((x+i-2),(y+j-2),root->level/2.0,root->level/2.0,root->level/2.0,root,2);
-       //printf("POINT to verify:(%d,%d)\n",x+i-2,y+j-2);
-      if(flag == 1 && insert->node.leaf.ship->bitmap[i][j] == NOT_HIT &&
-        (aux->node.leaf.p->x != (x+i-2) || aux->node.leaf.p->y != (y+j-2))){
-          points[clear].x = (x+i-2);
-          points[clear].y = (y+j-2);
-          clear++;
-          //printf("clear=%d\n",clear);
-        }
-    }
-  }
-  return clear;
-}
-
-//insert_ship
-void insert_ship(POINT* p,POINT* points, SHIP* ship, QD_NODE * root){
-  QD_NODE* insert = create_node(root->level/2.0);
-  *insert->node.leaf.p = *p;
-  *insert->node.leaf.ship = *ship;
-  //printf("Insert(%d,%d)\n",insert->node.leaf.p->x,insert->node.leaf.p->y);
-  //verificar se pode ser inserido
-  if(verify_insert(insert,root,points) != ship->size){
-    printf("\033[1;31m");
-    printf("ERROR! You can't insert the boat here!\n");
-    printf("\033[1;33m");
-    printf("Please choose another position.\n");
-    printf("X: "); scanf("%d",&p->x);
-    printf("Y: "); scanf("%d",&p->y);
-    printf("\n");
-    insert_ship(p,points,ship,root);
-  }
-  else{
-    //POINT *point_array = (POINT*) malloc(ship->size*sizeof(POINT));
-    //point_array=create_points(p,ship);
-    //printf("point:(%d,%d)\n",insert->node.leaf.p->x,insert->node.leaf.p->y);
-    for(int i=0 ;i<ship->size;i++){
-      //printf("node to insert:(%d,%d)\n",points[i].x,points[i].y);
-      *insert->node.leaf.p = points[i];
-      (void)node_insert(root,insert);
-
-    }
-    //free(point_array);
-    //point_array = NULL;
-  }
-}
-
-int generate_number(int a,int b){
-  return (a == 0)? rand()% ++b:rand() % ++b + a;
-}
-
-void rand_insert_ships(QD_NODE* root1,QD_NODE* root2){
-  int boat_types[]={2,3,4,5,7,9};
-  int map_size,rotation,verify;
-  map_size = root1->level;
-  int n_for_each_boat[6] = {1,1,1,1,1,1};
-
-  //printf("flag 1");
-
-  for(int i=0;i<6;i++){
-    for(int j=0;j<n_for_each_boat[i];j++){
-      //printf("i:%d\n",i);
-      //create ships
-      SHIP* newship1 = (SHIP*) malloc(sizeof(SHIP));
-      SHIP* newship2 = (SHIP*) malloc(sizeof(SHIP));
-      POINT* p1 = make_point(0,0);
-      POINT* p2 = make_point(0,0);
-      rotation = generate_number(0,3);
-      create_ship(newship1,rotation,boat_types[i]);
-      create_ship(newship2,rotation,boat_types[i]);
-      print_boat(newship1);
-      print_boat(newship2);
-
-      //get random coordinates
-      p1->x = generate_number(0,(map_size));
-      p1->y = generate_number(0,(map_size));
-      p2->x = generate_number(0,(map_size));
-      p2->y = generate_number(0,(map_size));
 
 
-      POINT *point_array1 = (POINT*) malloc(newship1->size*sizeof(POINT));
-      POINT *point_array2 = (POINT*) malloc(newship2->size*sizeof(POINT));
-
-      QD_NODE* n1 = create_node(root1->level);
-       *n1->node.leaf.p = *p1;
-       *n1->node.leaf.ship = *newship1;
-      QD_NODE* n2 = create_node(root2->level);
-       *n2->node.leaf.p = *p2;
-       *n2->node.leaf.ship = *newship2;
-
-      while((verify=verify_insert(n1,root1,point_array1)) != newship1->size){
-        n1->node.leaf.p->x = generate_number(0,(map_size));
-        n1->node.leaf.p->y = generate_number(0,(map_size));
-      }
-
-      while((verify=verify_insert(n2,root2,point_array2)) != newship2->size){
-        n2->node.leaf.p->x = generate_number(0,(map_size));
-        n2->node.leaf.p->y = generate_number(0,(map_size));
-      }
-
-      *p1 = *n1->node.leaf.p;
-      *p2 = *n2->node.leaf.p;
-      //p1->x = n1->node.leaf.p->x;
-      //p1->y = n1->node.leaf.p->y;
-      //p2->x = n2->node.leaf.p->x;
-      //p2->y = n2->node.leaf.p->y;
-
-      //insert
-      insert_ship(p1,point_array1,newship1,root1);
-      insert_ship(p2,point_array2,newship2,root2);
-
-      free(p1);
-      free(p2);
-      p1 = NULL;
-      p2 = NULL;
-       //printf("flag 5");
-    }
-  }
-}
-
-//Users
-void user_insert(GAME* g){
-  int boat_types[]={2,3,4,5,7,9};
-  char *boats[]={"Destroyer","Cruiser","Battleship","Carrier","Sigma","Pickaxe"};
-  int x,y,boat,rotation,boat_rotation,player;
-  player = 1;
-  system("clear");
-  printf("\033[1;31m");
-  printf("Time to insert your boats!\nRemember: \nX->rows, Y->cols, (y,x)->boat's center\nFor rotations you have:\n");
-  printf(" ___________\n");
-  printf("|           |\n");
-  printf("| 0 -> 0º   |\n");
-  printf("| 1 -> 90º  |\n");
-  printf("| 2 -> 180º |\n");
-  printf("| 3 -> 270º |\n");
-  printf("|___________|\n");
-  printf("Press any key to ENTER");
-  getchar();
-
-  for(;player<=2;player++){
-    for(int i=0;i<6;i++){
-      for(int j=0;j<n_for_each_boat[i];j++){
-        system("clear");
-        SHIP* newship = (SHIP*) malloc(sizeof(SHIP));
-
-        //print map during insertion
-        if(player == 1) print_game(g->map1,g->size);
-        else print_game(g->map2,g->size);
-
-        printf("\033[1;35m");
-        printf("               Player %d: \n",player);
-        printf("\033[1;33m");
-        printf("|--------------------------------------|\n");
-        printf("%s\n",(char*)boats[i]);
-
-        //get valid rotation
-        printf("Select Rotation: ");
-        scanf("%d",&boat_rotation);
-        while(boat_rotation < 0 || boat_rotation > 3){
-          printf("\033[1;31m");
-          printf("Invalid rotation. Insert new one: ");
-          printf("\033[1;33m");
-          scanf("%d",&boat_rotation);
-        }
-        printf("X:");
-        x = read_buffer();
-        printf("Y:");
-        y = read_buffer();
-
-        //player 1 inserts('insert_ship' asks for new coordinates if needed)
-        if(player == 1){
-          create_ship(newship,boat_rotation,boat_types[i]);
-          insert_ship(x,y,newship,g->map1,g->size);
-        }
-        //player2 inserts('insert_ship' asks for new coordinates if needed)
-        else{
-          create_ship(newship,boat_rotation,boat_types[i]);
-          insert_ship(x,y,newship,g->map2,g->size);
-        }
-      }
-    }
-  }
-}
-
-int main(){
+/*int main(){
   srand(time(NULL));
   int l = 19;
   QD_NODE* root1 = create_node(l);
@@ -666,8 +404,6 @@ int main(){
   POINT* p2 = make_point(7,4);
   POINT* p3 = make_point(7,5);
   POINT* p4 = make_point(7,6);
-
-
   SHIP* boat =(SHIP*) malloc (sizeof(SHIP));
   create_ship(boat,2,7);
   SHIP* boat1 =(SHIP*) malloc (sizeof(SHIP));
@@ -678,54 +414,42 @@ int main(){
   create_ship(boat3,1,9);
   SHIP* boat4 =(SHIP*) malloc (sizeof(SHIP));
   create_ship(boat4,1,9);
-
   QD_NODE* root = create_node(l);
   root->node.leaf.p = p;
   root->node.leaf.ship = boat;
   root->cx/=2;
   root->cy/=2;
-
-
   QD_NODE* d = create_node(l);
   *d->node.leaf.p = *p1;
   *d->node.leaf.ship = *boat1;
-
   QD_NODE* d1 = create_node(l);
   *d1->node.leaf.p = *p2;
   *d1->node.leaf.ship = *boat2;
-
   QD_NODE* d2 = create_node(l);
   *d2->node.leaf.p = *p3;
   *d2->node.leaf.ship = *boat3;
-
   QD_NODE* d3 = create_node(l);
   *d3->node.leaf.p = *p4;
   *d3->node.leaf.ship = *boat4;
-
   node_insert(root,d);
   //print_tree(root,l);
   //printf("END.LLL.%d\n",l);
-
   node_insert(root,d1);
   //print_tree(root,l);
   //printf("END.LLL.%d\n",l);
-
   node_insert(root,d2);
   //print_tree(root,l);
   //printf("END.LLL.%d\n",l);
-
   node_insert(root,d3);
   //print_tree(root,l);
   //printf("END.LLL.%d\n",l);
-
+  print_tree(root,l);
+  printf("END\n");
+  printf("nodes inside root:%d\n",root->nodes_inside);
+  node_delete(root,d3->node.leaf.p->x,d3->node.leaf.p->y,root->level);
   print_tree(root,l);
   printf("END\n");
 
-  printf("nodes inside root:%d\n",root->nodes_inside);
-  node_delete(root,d3->node.leaf.p->x,d3->node.leaf.p->y,root->level);
-
-  print_tree(root,l);
-  printf("END\n");*/
-
   return 0;
-}
+} 
+*/
